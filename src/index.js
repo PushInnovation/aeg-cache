36e9426e5cc1a09097840a0290e5dbc492cafc68
Justin Durham <justin.durham@icloud.com>
2016-09-30 10:46:11 -0400

    Updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Cache from './cache';
-import CacheExpiry from './cache-expiry';
+import * as CacheExpiry from './cache-expiry';
 
 export default {Cache, CacheExpiry};
 
 export { Cache, CacheExpiry };
-
