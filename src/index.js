6709f0f23dcfcade0b912404c186025185e1fe17
Justin Durham <justin.durham@icloud.com>
2016-10-12 18:15:01 -0400

    Add reporting cache

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import Cache from './cache';
 import ReportingCache from './reporting-cache';
 import * as CacheExpiry from './cache-expiry';
+import * as CacheExpiryReporting from './cache-expiry-reporting';
 import * as CacheKeys from './cache-keys';
 
-export default {Cache, ReportingCache, CacheExpiry, CacheKeys};
+export default {Cache, ReportingCache, CacheExpiry, CacheExpiryReporting, CacheKeys};
 
-export { Cache, ReportingCache, CacheExpiry, CacheKeys };
+export { Cache, ReportingCache, CacheExpiry, CacheExpiryReporting, CacheKeys };
