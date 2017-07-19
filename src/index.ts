19e273304ec95238bed702f977480526cbaee057
Justin Durham <justin.durham@icloud.com>
2017-07-19 13:42:37 -0400

    Typescript!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import Cache from './cache';
 import * as CacheExpiry from './cache-expiry';
 import * as CacheKeys from './cache-keys';
-import { Transaction, Batch } from '@adexchange/aeg-redis';
 
-export default {Cache, CacheExpiry, CacheKeys, Transaction, Batch};
+export default {Cache, CacheExpiry, CacheKeys};
 
-export { Cache, CacheExpiry, CacheKeys, Transaction, Batch };
+export { Cache, CacheExpiry, CacheKeys };
